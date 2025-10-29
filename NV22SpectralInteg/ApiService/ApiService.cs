0afb448e478385ef93c058c2da2776019cefffa9
Alpesh Patel <alpesh.p@flatworldsolutions.in>
2025-10-29 22:27:10 +0530

    Kiosk Report paramater isssue solved

--- NV22SpectralInteg/ApiService/ApiService.cs
+++ NV22SpectralInteg/ApiService/ApiService.cs
@@ -153,8 +153,8 @@ public static class ApiService
             storeRegId = AppSession.KioskRegId,
             kioskId = AppSession.KioskId,
             settlementCode = settlementCode,
-            startTime = startTime.ToString("yyyy-MM-ddTHH:mm:00"),
-            endTime = endTime.ToString("yyyy-MM-ddTHH:mm:00"),
+            startTime = startTime.ToString("yyyy-MM-ddTHH:mm"),
+            endTime = endTime.ToString("yyyy-MM-ddTHH:mm"),
             totalDenominationDepository = aggregatedData.totalDenominationDepository,
             totalSettlementAmount = aggregatedData.totalSettlementAmount,
             kioskIpAddress = currentLocalIp
